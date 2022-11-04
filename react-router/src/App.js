5fc3cdae103ee76a7938abafd2e6258eee66126f
Dan <dan@coderaider.tech>
2022-11-03 19:32:29 -0500

    Added bootstrap

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
@@ -15,22 +18,27 @@ function App() {
     return (
       <div className="App">
         <Router>
-            <header>
-              <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-                <div className="navBar">
-                    <ul>
-                      <li>
+            <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+            <Container>
+              <Nav defaultActiveKey="/" variant="tabs" fill>
+                <Nav.Item>
+                    <Nav.Link eventKey={"homePage"} href="/"> 
                       <Link to="/">Home</Link>
-                      </li>
-                      <li>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey={"aboutPage"} href="/about"> 
                       <Link to="/about">About Us</Link>
-                      </li>
-                      <li>
-                      <Link to="/packages">Our Packages</Link>
-                      </li>
-                    </ul>
-                </div>
-            </header>
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey={"packagesPage"} href="/packages"> 
+                      <Link to="/packages">Our Packages</Link> 
+                    </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Container>
+
         <div className="display">
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -47,3 +55,4 @@ function App() {
   
   export default App;
   
+
